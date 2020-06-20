5b52ded91fc17142a315f548af2c2e153c088f79
ysubbareddy9842 <67203120+ysubbareddy9842@users.noreply.github.com>
2020-06-20 23:47:38 +0530

    Add files via upload

---
+++ perimeter_of_square.py
@@ -0,0 +1,3 @@
+a=float(input("enter side of square"))
+p=4*a
+print("perimeter of square",p)
---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a**b
+h=a%b
+i=a//b
+print("the sum is",c)
+print("the difference is",d)
+print("product is",e)
+print("division is",f)
+print("power is",g)
+print("remainder is",h)
+print("quotient is",i)
